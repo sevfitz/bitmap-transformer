b248882c81945af0876e2e8ed5f8c7aea5daf19b
Mark Petrie <petrie.mark@gmail.com>
2017-10-03 00:06:25 -0700

    Minor cleanup and formatting of files

--- tests/bitmap-transformer.test.js
+++ tests/bitmap-transformer.test.js
@@ -4,26 +4,17 @@ const invert = require('../lib/invert-transformer');
 const fs = require('file-system');
 const beforeTest = require('../lib/before-test');
 
-
-
 describe('bitmap file transformer', () => {
-    
     let buffer = null;
     
     before(() => {
-       
         buffer = beforeTest(); 
-
     });
 
     it('test whole transform', () => {
-       
         const bitmap = new BitmapTransformer(buffer);
-
         bitmap.transform(invert);
-        
         const buffer2 = fs.readFileSync('./tests/inverted-expected.bmp');
-
         assert.deepEqual(bitmap.buffer, buffer2);
     });
 });
